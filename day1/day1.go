5c605a55456dc997b53cf29a9fb6545eeff48f1a
James Hageman <jamesd@hageman.ca>
2018-12-01 14:48:33 -0500

    refactor day1

--- day1/day1.go
+++ day1/day1.go
@@ -5,33 +5,34 @@ import (
 	"io"
 )
 
-func Part1(input io.Reader) int {
-	var sum, i int
+func readInts(input io.Reader) []int {
+	ints := make([]int, 0)
+	var x int
 	for {
-		_, err := fmt.Fscanf(input, "%d", &i)
+		_, err := fmt.Fscanf(input, "%d", &x)
 		if err != nil {
 			break
 		}
+		ints = append(ints, x)
+	}
+
+	return ints
+}
+
+func Part1(input io.Reader) int {
+	var sum int
+	for _, i := range readInts(input) {
 		sum += i
 	}
 	return sum
 }
 
 func Part2(input io.Reader) int {
-	var sum, x int
+	var sum int
 	sums := make(map[int]int)
-	inputs := make([]int, 0)
+	inputs := readInts(input)
 
 	sums[0] = 1
-
-	var err error
-	for {
-		_, err = fmt.Fscanf(input, "%d", &x)
-		if err != nil {
-			break
-		}
-		inputs = append(inputs, x)
-	}
 
 	i := 0
 	for {
