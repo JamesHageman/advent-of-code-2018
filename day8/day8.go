b7d82dafa62ded3f4212fa3c18f953a8c6e0cb82
James Hageman <jamesd@hageman.ca>
2018-12-09 14:58:32 -0500

    finish day 8

--- day8/day8.go
+++ day8/day8.go
@@ -45,6 +45,22 @@ func sumMeta(node *Node) int {
 	return sum
 }
 
+func getValue(node *Node) int {
+	if len(node.children) == 0 {
+		return sumMeta(node)
+	}
+
+	sum := 0
+	for _, d := range node.metadata {
+		if d < 1 || d > len(node.children) {
+			continue
+		}
+		child := node.children[d-1]
+		sum += getValue(child)
+	}
+	return sum
+}
+
 func Part1(input io.Reader) int {
 	root := scan(input)
 	return sumMeta(root)
@@ -52,5 +68,5 @@ func Part1(input io.Reader) int {
 
 func Part2(input io.Reader) int {
 	root := scan(input)
-	return sumMeta(root)
+	return getValue(root)
 }
--- day8/day8_test.go
+++ day8/day8_test.go
@@ -20,12 +20,15 @@ func TestPart1(t *testing.T) {
 	}
 	x = day8.Part1(file)
 	t.Logf("Part 1: %d", x)
+	if x != 41521 {
+		t.Fatalf("wrong answer")
+	}
 }
 
 func TestPart2(t *testing.T) {
 	x := day8.Part2(bytes.NewBufferString("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"))
-	if x != 138 {
-		t.Errorf("expected x = 138, got %d", x)
+	if x != 66 {
+		t.Errorf("expected x = 66, got %d", x)
 	}
 
 	file, err := os.Open("./day8.txt")
@@ -33,5 +36,8 @@ func TestPart2(t *testing.T) {
 		t.Error(err)
 	}
 	x = day8.Part2(file)
-	t.Logf("Part 1: %d", x)
+	t.Logf("Part 2: %d", x)
+	if x != 19990 {
+		t.Fatalf("wrong answer")
+	}
 }
